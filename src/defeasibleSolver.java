a8b0ca26a12fc7942cd150b13b6c68b91c7977dc
Jordyn <35696204+ColourfulMelon@users.noreply.github.com>
2021-11-07 10:06:11 +0200

    gui

--- src/defeasibleSolver.java
+++ src/defeasibleSolver.java
@@ -1,13 +1,20 @@
 import javax.swing.*;
 
 public class defeasibleSolver {
+    private JFrame frame;
+    private JLabel label;
+    private  JPanel panel;
+
     public GUI(){
-        JFrame frame = new JFrame();
-        Jpanel panel = new JPanel();
+        frame = new JFrame();
+        panel = new JPanel();
     }
+
+
     public static void main(String[] args) {
         new GUI();
     }
 
 
 }
+
