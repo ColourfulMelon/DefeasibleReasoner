67f73140746a7227c16434747020f0e0396445a8
ColourfulMelon <35696204+Jay078@users.noreply.github.com>
2021-11-11 03:03:08 +0200

    Updates

--- src/defeasibleReasoner.java
+++ src/defeasibleReasoner.java
@@ -2,34 +2,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class defeasibleReasoner implements ActionListener {
     private JFrame frame;
     private  JPanel panel;
+    private JPanel right;
     private JButton next;
-    private JButton add;
-    private JTextField input;
-    private JLabel title;
-    private JLabel enter_noun;
-
+    private JButton add = new JButton("Add");
+    private JTextField input = new JTextField();
+    private JLabel title = new JLabel("Defeasible Reasoner");
+    private JLabel enter_noun = new JLabel("Enter a Noun");
+    private JLabel nouns = new JLabel("Nouns:");
+    private  JList list;
+  //  private DefaultListModel listModel;
+    private JScrollPane scroll = new JScrollPane();
+    private List<String> objectList = new ArrayList<>(0);
 
     public defeasibleReasoner() {
-        frame = new JFrame();
-        panel = new JPanel();
-
         next = new JButton("Next");
         next.addActionListener(this);
+        next.setVerticalAlignment(JButton.BOTTOM);
+        next.setHorizontalAlignment(JButton.RIGHT);
+        next.setFocusable(false);
+        add.addActionListener(this);
+        add.setFocusable(false);
 
-        title = new JLabel();
+        input.setSize(400,30);
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,600,1200));
-        panel.setLayout(new GridLayout(0, 1));
+       // listModel = new DefaultListModel();
+        //list = new JList(listModel);
+        list = new JList<String>(objectList.toArray(new String[objectList.size()]));
+        list.setVisibleRowCount(-1);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        //list.setPreferredSize(new Dimension(300,500));
+        //list.setPreferredSize(new Dimension(300,500));
+        list.setBackground(Color.ORANGE);
+        scroll.setViewportView(list);
+        scroll.getViewport().setOpaque(false);
+        list.setLayoutOrientation(JList.VERTICAL);
+
+
+        panel = new JPanel();
+        //panel.setBorder(BorderFactory.createEmptyBorder(30,30,600,800));
+        panel.setLayout(null);
+        panel.setBounds(0,0,500,1000);
+        panel.add(title);
+        panel.add(enter_noun);
+        panel.add(input);
+        panel.add(add);
         panel.add(next);
+        panel.setBackground(Color.blue);
 
+        right = new JPanel();
+        //right.setBorder(BorderFactory.createEmptyBorder(30,30,600,200));
+        right.setLayout(null);
+        right.add(nouns);
+        right.add(scroll);
+        right.setBackground(Color.GREEN);
+        //panel.add(right, BorderLayout.EAST);
+
+        frame = new JFrame();
         frame.add(panel, BorderLayout.CENTER);
+        frame.add(right, BorderLayout.EAST);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Defeasible Solver");
-        frame.pack();
+        frame.setSize(1000,800);
+        //frame.pack();
         frame.setVisible(true);
     }
 
@@ -41,8 +81,16 @@ public class defeasibleReasoner implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == next){
+            System.out.println("Next button pressed");
+        }
+        else if (e.getSource() == add){
+            System.out.println("Add button pressed");
+           // listModel.addElement(input.getText());
+            objectList.add(input.getText());
 
         }
+
+    }
 }
 
